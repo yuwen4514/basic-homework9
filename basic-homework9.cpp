30dde777fe5f95291c7f8d07c3f755a41c218e24
yuwen4514 <48576822+yuwen4514@users.noreply.github.com>
2019-04-12 00:43:43 +0800

    Add files via upload

---
+++ basic-homework9.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void) 
+{ 
+ int x,y;
+ for(x=1;x<=9;x++)
+ {
+   for(y=1;y<=9;y++)
+   printf("%d*%d=%2d  ",x,y,y*x);
+   printf("\n");
+ }
+
+system("pause");
+return 0;
+}
